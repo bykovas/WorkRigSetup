0a70289d580691aae113338fdf82ebf2f6ff3b18
Denisas Bykovas <bykovas@icloud.com>
2023-07-26 14:54:10 +0300

    Add files via upload

---
+++ Home WindowsPC Developer Configuration.ps1
@@ -0,0 +1,10 @@
+﻿#Set-ExecutionPolicy -ExecutionPolicy unrestricted
+#iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+
+Enable-WindowsOptionalFeature -FeatureName Microsoft-Windows-Subsystem-Linux -Online -NoRestart
+Enable-WindowsOptionalFeature -FeatureName Microsoft-Hyper-V-All -Online -NoRestart
+Enable-WindowsOptionalFeature -FeatureName Containers -Online -NoRestart
+
+#Development
+choco install visualstudio2022enterprise -y
+choco install VisualStudioCode -y
